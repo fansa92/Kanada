6505f3407ef8d35619ad3081323c4c2a15a94260
xiaocaoooo <904228000@qq.com>
2025-05-17 06:26:45 +0800

    [add] 实现歌词页进度条

--- lib/pages/lyric.dart
+++ lib/pages/lyric.dart
@@ -18,9 +18,13 @@ class LyricPage extends StatefulWidget {
 class _LyricPageState extends State<LyricPage> {
   String? path;
   Metadata? metadata;
+  double _duration = 1;
+  double _position = 0;
+  double _progress = 0;
 
   // StreamSubscription<int?>? _currentIndexSub;
   StreamSubscription<SequenceState?>? _sequenceSub;
+  StreamSubscription<Duration>? _positionSub;
 
   @override
   void initState() {
@@ -37,12 +41,20 @@ class _LyricPageState extends State<LyricPage> {
         _init();
       }
     });
+    // 初始化位置监听
+    _positionSub = Global.player.positionStream.listen((position) {
+      _duration = Global.player.duration?.inMilliseconds.toDouble() ?? 1.0;
+      _position = Global.player.position.inMilliseconds.toDouble();
+      _progress = _duration > 0 ? _position / _duration : 0.0;
+      if (mounted) setState(() {});
+    });
   }
 
   @override
   void dispose() {
     // _currentIndexSub?.cancel();
     _sequenceSub?.cancel();
+    _positionSub?.cancel();
     super.dispose();
   }
 
@@ -76,87 +88,129 @@ class _LyricPageState extends State<LyricPage> {
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        metadata?.path != null
-            ? LyricView(
-              path: metadata!.path,
-              paddingTop: 100 + MediaQuery.of(context).padding.top,
-              // paddingBottom: MediaQuery.of(context).size.height,
-              // padding: EdgeInsets.only(
-              //   top: 100 + MediaQuery.of(context).padding.top,
-              // ),
-            )
-            : Container(),
+        Padding(
+          padding: EdgeInsets.only(
+            top: 100 + MediaQuery.of(context).padding.top,
+          ),
+          child:
+              metadata?.path != null
+                  ? LyricView(
+                    path: metadata!.path,
+                    paddingTop: 100 + MediaQuery.of(context).padding.top,
+                    // paddingBottom: MediaQuery.of(context).size.height,
+                    // padding: EdgeInsets.only(
+                    //   top: 100 + MediaQuery.of(context).padding.top,
+                    // ),
+                  )
+                  : Container(),
+        ),
         SizedBox(
           height: 100 + MediaQuery.of(context).padding.top,
           width: double.infinity,
           child: ClipRect(
-            child: BackdropFilter(
-              filter: ImageFilter.blur(sigmaX: 8, sigmaY: 8),
+            // child: BackdropFilter(
+            //   filter: ImageFilter.blur(sigmaX: 8, sigmaY: 8),
+            child: Container(
+              color: Theme.of(
+                context,
+              ).colorScheme.surface.withValues(alpha: .8),
               child: Padding(
                 padding: EdgeInsets.only(
                   top: MediaQuery.of(context).padding.top,
                 ),
-                child: Row(
+                child: Stack(
                   children: [
-                    Padding(
-                      padding: EdgeInsets.only(left: 25),
-                      child: Card(
-                        elevation: 0,
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(8),
-                          child: SizedBox(
-                            width: 80,
-                            height: 80,
-                            child:
-                                metadata?.picture != null
-                                    ? Image.memory(metadata!.picture!)
-                                    : (metadata?.pictureCache != null
-                                        ? Image.file(
-                                          File(metadata!.pictureCache!),
-                                        )
-                                        : (Global.pictureCache != null
+                    Row(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(left: 25),
+                          child: Card(
+                            elevation: 0,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(8),
+                              child: SizedBox(
+                                width: 80,
+                                height: 80,
+                                child:
+                                    metadata?.picture != null
+                                        ? Image.memory(metadata!.picture!)
+                                        : (metadata?.pictureCache != null
                                             ? Image.file(
-                                              File(Global.pictureCache!),
+                                              File(metadata!.pictureCache!),
                                             )
-                                            : Icon(Icons.music_note))),
+                                            : (Global.pictureCache != null
+                                                ? Image.file(
+                                                  File(Global.pictureCache!),
+                                                )
+                                                : Icon(Icons.music_note))),
+                              ),
+                            ),
                           ),
                         ),
+                        SizedBox(width: 10),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                metadata?.title ??
+                                    path?.split('/').last ??
+                                    'Unknown Title',
+                                style: Theme.of(
+                                  context,
+                                ).textTheme.titleLarge?.copyWith(
+                                  color: Theme.of(
+                                    context,
+                                  ).colorScheme.onSurface.withValues(alpha: .8),
+                                ),
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                              SizedBox(height: 5),
+                              Text(
+                                metadata?.artist ?? 'Unknown Artist',
+                                style: Theme.of(
+                                  context,
+                                ).textTheme.bodyMedium?.copyWith(
+                                  color: Theme.of(
+                                    context,
+                                  ).colorScheme.onSurface.withValues(alpha: .6),
+                                ),
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      left: 0,
+                      right: 0,
+                      child: Container(
+                        height: 1,
+                        color: Theme.of(
+                          context,
+                        ).colorScheme.onSurface.withValues(alpha: .1),
                       ),
                     ),
-                    SizedBox(width: 10),
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            metadata?.title ??
-                                path?.split('/').last ??
-                                'Unknown Title',
-                            style: Theme.of(
-                              context,
-                            ).textTheme.titleLarge?.copyWith(
-                              color: Theme.of(
-                                context,
-                              ).colorScheme.onSurface.withValues(alpha: .6),
-                            ),
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
+                    Positioned(
+                      bottom: 0,
+                      left: 0,
+                      right: 0,
+                      child: Container(
+                        height: 3,
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [
+                              Theme.of(context).colorScheme.primary,
+                              Colors.transparent,
+                            ],
+                            stops: [_progress-0.005, _progress+0.005],
                           ),
-                          SizedBox(height: 5),
-                          Text(
-                            metadata?.artist ?? 'Unknown Artist',
-                            style: Theme.of(
-                              context,
-                            ).textTheme.bodyMedium?.copyWith(
-                              color: Theme.of(
-                                context,
-                              ).colorScheme.onSurface.withValues(alpha: .6),
-                            ),
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ],
+                        ),
                       ),
                     ),
                   ],
